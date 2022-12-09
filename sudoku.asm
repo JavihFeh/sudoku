f4809e70016333c4a066a1f507bae1f1b3568db6
JavihFeh <37805872+JavihFeh@users.noreply.github.com>
2022-12-09 10:21:37 -0300

    sud0.9

--- sudoku.asm
+++ sudoku.asm
@@ -57,38 +57,16 @@ pula_linha MACRO
 ENDM 
 
 imp_sudoku MACRO matriz
-    XOR BX,BX
-    volta:
-    XOR DI,DI
-    MOV CX,9
-    imp_matriz:
-        MOV AH,02
-        MOV DL, matriz[BX][DI]
-        INT 21h
-        INC DI
-        Mov ah,09
-        Lea dx,msg2
-        INT 21h
-        espaco
-        pula:
-    loop imp_matriz
-    pula_linha
-    CMP BX,72
-    JE fim
-    ADD BX,9
-    Mov ah,09
-    Lea dx ,msg3
-    INT 21h 
-    JMP volta
-    fim:
+    
 ENDM 
 .data
-    msg2 DB  OC4H , OC5H , 18 DUP (OC4H), '$'
-    msg3 DB  OB3H , '$'
+    win DB 'Parabens! Voce completou o sudoku... $'
+    msg2 DB  0C4H,22 DUP (0C4H),'$'
+    msg3 DB  0B3H,'$'
     go DB 10,'Game over! Suas vidas acabaram...$'
     vida_loss DB 'Voce perdeu uma vida! $'
-    msg_vida DB 'Vidas: '
-    vidas DB 3
+    msg_vida DB 'Vidas: $'
+    vidas DB '3$'
     msg_erro DB 'Erro!$'
     res DB 'Qual a resposta? $'
     msg_lin DB 'Qual a linha? $'
@@ -101,6 +79,27 @@ ENDM
     ;D - Dificil
     ;N - Normal
     ;R - Reposta
+
+    NR  DB '4','1','5','3','7','8','2','9','6'
+        DB '2','3','7','1','6','9','4','8','5'
+        DB '6','8','9','4','2','5','7','3','1'
+        DB '8','9','3','7','5','1','6','2','4'
+        DB '5','7','4','2','3','6','9','1','8'
+        DB '1','6','2','9','8','4','3','5','7'
+        DB '9','2','8','6','1','7','5','4','3'
+        DB '7','4','1','5','9','3','8','6','2'
+        DB '3','5','6','8','4','2','1','7','9$'
+
+    N   DB '4','?','?','3','?','8','?','?','6'
+        DB '2','3','?','?','6','?','4','?','?'
+        DB '?','?','9','4','?','?','7','?','?'
+        DB '8','9','?','7','?','?','?','?','?'
+        DB '5','?','?','?','?','?','9','1','?'
+        DB '?','6','?','?','?','?','?','?','7'
+        DB '?','?','8','?','1','?','?','4','3'
+        DB '?','4','1','?','?','?','?','6','?'
+        DB '?','?','?','8','?','2','?','7','?$'
+
   DR    DB '5','3','4','6','7','8','9','1','2'
         DB '6','7','2','1','9','5','3','4','8'
         DB '1','9','8','3','4','2','5','6','7'
@@ -111,7 +110,7 @@ ENDM
         DB '2','8','7','4','1','9','6','3','5'
         DB '3','4','5','2','8','6','1','7','9$'
 
- D      DB '5','3','?','?','7','?','?','?','?'
+    D   DB '5','3','?','?','7','?','?','?','?'
         DB '6','?','?','1','9','5','?','?','?'
         DB '?','9','8','?','?','?','?','6','?'
         DB '8','?','?','?','6','?','?','?','3'
@@ -119,7 +118,7 @@ ENDM
         DB '7','?','?','?','2','?','?','?','6'
         DB '?','6','?','?','?','?','2','8','?'
         DB '?','?','?','4','1','9','?','?','5'
-        DB '?','?','?','?','8','?','?','7','9','$'
+        DB '?','?','?','?','8','?','?','7','9$'
 .code
 MAIN PROC
     MOV AX, @data
@@ -128,31 +127,43 @@ MAIN PROC
 
     LIMPA_TELA
 
-    MOV AH ,09
+    MOV AH,09
     LEA DX,MSG1
     INT 21h
 
-  pula_linha 
+    pula_linha 
 
+    MOV AH,09
     LEA DX,msg_dif
     INT 21h
 
     MOV AH,07
     INT 21h
 
+    MOV dificuldade,AL
+
     continuar:
     MOV AH,09
     LEA DX,msg_vida
     INT 21h
 
-    MOV AH,02
-    MOV AL,vidas
-    AND AL, 30h
+    MOV AH,09
+    LEA DX,vidas
     INT 21h
 
     CALL IMPRESSAO
     CALL PREENCHER
 
+    CMP dificuldade, 31h
+    JNE vef_d
+    MOV CX,89
+    CLD
+    LEA DI,N
+    MOV AL,'?'
+    REPNE SCASB
+    JE continuar
+
+    vef_d:
     MOV CX,89
     CLD
     LEA DI,D
@@ -160,6 +171,9 @@ MAIN PROC
     REPNE SCASB
     JE continuar
 
+    MOV AH,09
+    LEA DX, win
+    INT 21h
 
     MOV AH, 4Ch
     INT 21h
@@ -172,18 +186,75 @@ MAIN PROC
 
     MOV AH,07
     INT 21h
+
+    MOV AH, 4Ch
+    INT 21h
 MAIN ENDP
 
 IMPRESSAO PROC
     PUSHREG
     pula_linha
-    TEST AL, 1
-    JZ dificil
-    JMP ret_imp
+    CMP dificuldade, 31h
+    JNE dificil
+    JMP normal
     dificil:
-    imp_sudoku D
-    ret_imp:
-    POPREG 
+    XOR BX,BX
+    voltad:
+    ; MOV AH,09
+    ; LEA DX, msg2
+    ; INT 21h
+    pula_linha
+    ; MOV AH,09
+    ; LEA DX,msg3
+    ; INT 21h
+    XOR DI,DI
+    MOV CX,9
+    imp_dificil:
+        MOV AH,02
+        MOV DL, D[BX][DI]
+        INT 21h
+        INC DI
+        ; MOV AH,09
+        ; LEA DX,msg3
+        ; INT 21h
+        espaco
+    loop imp_dificil
+    pula_linha
+    CMP BX,72
+    JE fim
+    ADD BX,9
+    JMP voltad
+    pula_linha
+
+    normal:
+    XOR BX,BX
+    voltan:
+    ; MOV AH,09
+    ; LEA DX, msg2
+    ; INT 21h
+    pula_linha
+    ; MOV AH,09
+    ; LEA DX,msg3
+    ; INT 21h
+    XOR DI,DI
+    MOV CX,9
+    imp_normal:
+        MOV AH,02
+        MOV DL, N[BX][DI]
+        INT 21h
+        INC DI
+        ; MOV AH,09
+        ; LEA DX,msg3
+        ; INT 21h
+        espaco
+    loop imp_normal
+    pula_linha
+    CMP BX,72
+    JE fim
+    ADD BX,9
+    JMP voltan
+    fim:
+    POPREG
     RET
 IMPRESSAO ENDP
 
@@ -193,7 +264,7 @@ PREENCHER PROC
     LEA DX,msg_lin
     INT 21h
 
-    MOV AH, 07
+    MOV AH, 01
     INT 21h
 
     XOR AH, AH
@@ -210,7 +281,7 @@ PREENCHER PROC
     LEA DX,msg_col
     INT 21h
 
-    MOV AH, 07
+    MOV AH, 01
     INT 21h
 
     XOR AH, AH
@@ -220,21 +291,25 @@ PREENCHER PROC
 
     pula_linha
 
-    CMP D[BX][DI],63
-    JNE jmp_erro
-    
+    CMP dificuldade,31h
+    JNE preen_dif
+    CMP N[BX][DI],63
+    JE cont_pren_nor
+    JMP jmp_erro
+
+    cont_pren_nor:
     pula_linha
 
     MOV AH,09
     LEA DX,res
     INT 21h
 
-    MOV AH,07
+    MOV AH,01
     INT 21h
 
-    CMP AL,DR[BX][DI]
-    JE sem_erro
-    SUB vidas,1
+    CMP AL,NR[BX][DI]
+    JE sem_erron
+    DEC vidas
     pula_linha
     MOV AH, 09
     LEA DX, vida_loss
@@ -247,7 +322,39 @@ PREENCHER PROC
     JGE encerrar
     JMP fimdejogo
 
-    sem_erro:
+    sem_erron:
+    MOV N[BX][DI],AL
+    JMP encerrar
+
+    preen_dif:
+    CMP D[BX][DI],63
+    JNE jmp_erro
+    
+    pula_linha
+
+    MOV AH,09
+    LEA DX,res
+    INT 21h
+
+    MOV AH,01
+    INT 21h
+
+    CMP AL,DR[BX][DI]
+    JE sem_errod
+    DEC vidas
+    pula_linha
+    MOV AH, 09
+    LEA DX, vida_loss
+    INT 21h
+
+    MOV AH,07
+    INT 21h
+
+    CMP vidas,0
+    JGE encerrar
+    JMP fimdejogo
+
+    sem_errod:
     MOV D[BX][DI],AL
     JMP encerrar
 
